50b6b5eb6af90dda1dc6707e778b955ade7e2521
Bhavna Yadav <bhawanayadavsln@gmail.com>
2026-02-18 00:22:17 +0530

    Test case fix

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,10 +2,10 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 transform = transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
-def preprocess_image(path):
+def resize_image(path):
     image = Image.open(path).convert("RGB")
     return transform(image)
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,5 +1,17 @@
 from src.preprocess import resize_image
+from PIL import Image
+import os
 
-def test_resize():
-    img = resize_image("sample.jpg")
-    assert img.shape == (3,224,224)
+def test_preprocess_shape():
+    # Create a dummy image
+    img = Image.new("RGB", (300, 300), color="red")
+    img.save("temp_test_image.png")
+
+    # Run preprocess
+    tensor = resize_image("temp_test_image.png")
+
+    # Check shape
+    assert tensor.shape == (3, 224, 224)
+
+    # Clean up
+    os.remove("temp_test_image.png")
